8690303c40c524d38dcaa09d838740ad63b17a99
HenryGymShark <henry.tsang@gymshark.com>
2022-06-23 09:19:28 +0100

    Initial Commit

---
+++ ChuckNorrisJokes/ChuckNorrisJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisJokesApp.swift
+//  ChuckNorrisJokes
+//
+//  Created by Henry Tsang on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
